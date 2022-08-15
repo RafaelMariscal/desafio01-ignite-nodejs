ba5fba01dc954dbb54c7268fbb1883c7f60a7180
Rafael Mariscal <rafael_mariscal_@outlook.com>
2022-08-15 09:06:57 -0300

    Update/Methods get, post and put finished.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,93 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.body
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    request.user = user
+    return next()
+  }
+  return response.status(400).json({ error: 'User not found.' })
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body
+
+  const itUserAlreadyExists = users.some(user => user.username === username)
+  if (itUserAlreadyExists) {
+    return response.status(400).json({ error: 'Username already exists.' })
+  }
+
+  const newUser = {
+    id: uuidv4(),
+    name: name,
+    username: username,
+    todos: []
+  }
+  users.push(newUser)
+  return response.status(201).json({ newUser })
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  const { username } = request.headers
+
+  if (!username) {
+    return response.status(400).json({ error: 'No username found at request headers.' })
+  }
+
+  return user.todos
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  const { title, deadline } = request.body
+  const { username } = request.headers
+
+  if (!username) {
+    return response.status(400).json({ error: 'No username found at request headers.' })
+  }
+
+  const newTodo = {
+    id: uuidv4(),
+    title: title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+  user.todos.push(newTodo)
+
+  return response.status(201).json({ newTodo })
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request
+  const { id } = request.params
+  const { title, deadline } = request.body
+  const { username } = request.headers
+
+  if (!username) {
+    return response.status(400).json({ error: 'No username found at request headers.' })
+  }
+
+  const indexOfSelectedTodo = user.todos.indexOf(todo => todo.id === id)
+  if (!indexOfSelectedTodo || indexOfSelectedTodo === -1) {
+    return response.status(400).json({ error: 'Todo not found at Todos Array.' })
+  }
+  user.todos[indexOfSelectedTodo].title = title
+  user.todos[indexOfSelectedTodo].deadline = deadline
+
+  return response.status(201).send()
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
